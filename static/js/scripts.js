00d16a6448e4b52794714cbf3e50c5b4a962c4b7
inkyless <violalakwok@gmail.com>
2024-12-09 12:50:23 +0700

    Integrate Deep Learning Model to Flask App and add styles to result screen

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,25 +1,27 @@
 
 const ImageUpload = document.getElementById('images');
 const fileInput = document.querySelector("#output")
+const submitButton = document.getElementById("submit")
 fileInput.style.display = "none";
 
-
-ImageUpload.addEventListener('change', function() {
-   const file = ImageUpload.files[0];
+ImageUpload.addEventListener('change', function () {
+  const file = ImageUpload.files[0];
 
   const reader = new FileReader();
 
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     const imageDataUrl = e.target.result;
 
     const imagepreview = document.getElementById("image-preview")
-    const submitButton = document.getElementById("submit")
+    const resetButton = document.getElementById("reset")
     const submitBox = document.getElementById("submit-wrap")
 
-    imagepreview.innerHTML="Image Preview"
-    submitButton.style.display="block"
-    submitBox.style.visibility = 'visible'
-    submitButton.innerText="Submit to Check"
+    imagepreview.innerHTML = "Image Preview"
+    submitButton.style.visibility = "visible"
+    submitBox.style.display = 'flex'
+    submitButton.innerText = "Submit to Check"
+    resetButton.innerText = "Reset"
+    resetButton.style.visibility = "visible"
     fileInput.src = imageDataUrl;
     fileInput.style.display = "block";
 
@@ -27,3 +29,7 @@ ImageUpload.addEventListener('change', function() {
 
   reader.readAsDataURL(file);
 });
+
+
+
+
