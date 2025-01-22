feaeb8ef687be8c36a5012a37067b83243a56ccb
inkyless <violalakwok@gmail.com>
2025-01-23 00:03:52 +0700

    Finished implemeting CI/CD to deploying Kubernetes

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 from waitress import serve
 from app import app
-import os
+import sys
 
 if __name__ == "__main__":
-    serve(app, listen="*:8080",
+    serve(app,
           url_scheme="https",threads=4)
+    
+def application(env, start_response):
+    version = f"{sys.version_info.major}.{sys.version_info.minor}"
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    message = f"Waitress serving up WSGI Python {version} in a Docker container"
+    return [message.encode("utf-8")]
